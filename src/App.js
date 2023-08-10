2af191432ca1023913cd34ebc46c09c60760d49c
Shubham <ashutosh51198@gmail.com>
2023-08-10 16:00:24 +0530

    Data-Fetched From Github-API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import {BrowserRouter as Router } from 'react-router-dom'
-import Footer from "./components/Footer";
+
+import {BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Navbar from "./components/layout/Navbar";
+
 
 // 1. Deal with third party API  
 // 2. Dive more into the context API intead of using useState.
@@ -13,7 +18,14 @@ function App() {
     <Router>
      <div className="flex flex-col justify-between h-screen">
         <Navbar/>
-        <main className="container mx-auto  px-3 pb-12">Content</main>
+        <main className="container mx-auto  px-3 pb-12">
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/Notfound' element={<NotFound/>}/>
+            <Route path='/*' element={<NotFound/>}/>
+          </Routes>
+        </main>
         <Footer/>
      </div>
     </Router>
